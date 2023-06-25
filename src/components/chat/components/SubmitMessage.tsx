1561500c634a1290c9b433a8cf054504a387be4f
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-06-25 22:07:46 +0300

    submitting message is now migrated to the React query as well, although something is not working properly

--- src/components/chat/components/SubmitMessage.tsx
+++ src/components/chat/components/SubmitMessage.tsx
@@ -1,41 +1,97 @@
-import { useContext, useEffect } from "react"
-import { ChatContext } from '../../../context/ChatContext'
-import { auth } from "../../../firebase-config"
-import { useLocation } from "react-router-dom"
-import { FieldValue, serverTimestamp } from "firebase/firestore"
-import { useRef } from "react"
+import { useEffect } from "react";
+import useChatContext from "../../../hooks/useContextHooks/useChatContext";
+import { auth, db } from "../../../firebase-config";
+import { useLocation } from "react-router-dom";
+import {
+  FieldValue,
+  serverTimestamp,
+  collection,
+  addDoc,
+} from "firebase/firestore";
+import format from "date-fns/format";
+import { useRef } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type PropsType = {
-  username: string
-}
+  username: string;
+};
+
+type SubmitMutation = {
+  e: React.FormEvent<HTMLFormElement>;
+  username: string;
+  image: string | null | undefined;
+  newMessage: string;
+  timestamp: FieldValue;
+  normaluserpair: string;
+};
 
 const SubmitMessage = ({ username }: PropsType) => {
-  const { setNewMessage, newMessage, handleSubmit } = useContext(ChatContext)
+  const { setNewMessage, newMessage, handleSubmit } = useChatContext();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const queryClient = useQueryClient();
+  const { state } = useLocation();
 
-  const inputRef = useRef<HTMLInputElement | null>(null)
+  const image: string | null | undefined = auth.currentUser?.photoURL;
+  const name = state?.name;
+  const timestamp: FieldValue = serverTimestamp();
 
-  const image: string | null | undefined = auth.currentUser?.photoURL
-  const { state } = useLocation()
-  const name = state?.name
-  const timestamp: FieldValue = serverTimestamp()
-
-  const normaluserpair: string = `${username}-${name}`
-  const reverseduserpair: string = `${name}-${username}`
+  const normaluserpair: string = `${username}-${name}`;
+  const reverseduserpair: string = `${name}-${username}`;
 
   useEffect(() => {
-    localStorage.setItem("userpair", normaluserpair)
-    localStorage.setItem("reversed", reverseduserpair)
-  })
+    localStorage.setItem("userpair", normaluserpair);
+    localStorage.setItem("reversed", reverseduserpair);
+  });
+
+  const submitMessage = async (variables: SubmitMutation) => {
+    const { username, newMessage, normaluserpair, e } = variables;
+    e.preventDefault();
+    const docref = collection(db, "messages");
+    const displayDate = `${format(new Date(), "MMMM dd, yyyy pp")}`;
+    await addDoc(docref, {
+      username,
+      image,
+      newMessage,
+      timestamp,
+      normaluserpair,
+      displayDate,
+      id: "",
+    });
+    setNewMessage("");
+  };
+
+  const submitMutation = useMutation({
+    mutationFn: (variables: SubmitMutation) => submitMessage(variables),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["messages"]);
+    },
+  });
 
   return (
     <section className="chat__form">
-      <form 
-        onSubmit={(e) => handleSubmit(e, username, image, newMessage, timestamp, normaluserpair)}
+      <form
+        onSubmit={(e) =>
+          submitMutation.mutate({
+            e,
+            username,
+            image,
+            newMessage,
+            timestamp,
+            normaluserpair,
+          })
+        }
       >
-        <input ref={inputRef} className="chat__send" placeholder="Type your message here..." type="text" value={newMessage} onChange={(e) => setNewMessage(e.target.value)}/>
+        <input
+          ref={inputRef}
+          className="chat__send"
+          placeholder="Type your message here..."
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default SubmitMessage
+export default SubmitMessage;
--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,118 +1,148 @@
-import { createContext, ReactElement, useState, useCallback, useEffect } from "react";
+import {
+  createContext,
+  ReactElement,
+  useState,
+  useCallback,
+  useEffect,
+} from "react";
 import { db } from "../firebase-config";
-import { addDoc, collection, getDocs, query, where, orderBy, doc, deleteDoc, FieldValue } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  doc,
+  deleteDoc,
+  FieldValue,
+} from "firebase/firestore";
 import { format } from "date-fns";
+import type { SnapType, ChildrenType } from "../types/Types";
 
 type ChatContextType = {
-    handleSubmit: (e: React.FormEvent<HTMLFormElement>, creator: string, image: string | null | undefined, message: string, timestamp: any, userpair: string) => Promise<void>,
-    handleDelete: (id: string) => Promise<void>,
-    messages: SnapType[],
-    newMessage: string,
-    setNewMessage: React.Dispatch<React.SetStateAction<string>>,
-    userpair: string | null,
-    setUserpair: React.Dispatch<React.SetStateAction<string | null>>,
-    reversed: string | null,
-    setReversed: React.Dispatch<React.SetStateAction<string | null>>
-}
+  handleSubmit: (
+    e: React.FormEvent<HTMLFormElement>,
+    creator: string,
+    image: string | null | undefined,
+    message: string,
+    timestamp: any,
+    userpair: string
+  ) => Promise<void>;
+  handleDelete: (id: string) => Promise<void>;
+  messages: SnapType[];
+  newMessage: string;
+  setNewMessage: React.Dispatch<React.SetStateAction<string>>;
+  userpair: string | null;
+  setUserpair: React.Dispatch<React.SetStateAction<string | null>>;
+  reversed: string | null;
+  setReversed: React.Dispatch<React.SetStateAction<string | null>>;
+};
 
 const initstate = {
-    handleSubmit: async () => {},
-    handleDelete: async () => {},
-    messages: [],
-    newMessage: "",
-    setNewMessage: () => {},
-    userpair: "",
-    setUserpair: () => {},
-    reversed: "",
-    setReversed: () => {}
-}
+  handleSubmit: async () => {},
+  handleDelete: async () => {},
+  messages: [],
+  newMessage: "",
+  setNewMessage: () => {},
+  userpair: "",
+  setUserpair: () => {},
+  reversed: "",
+  setReversed: () => {},
+};
 
-export const ChatContext = createContext<ChatContextType>(initstate)
+export const ChatContext = createContext<ChatContextType>(initstate);
 
-type ChildrenType = {
-    children?: ReactElement | ReactElement[]
-}
+export const ChatProvider = ({ children }: ChildrenType) => {
+  const [messages, setMessages] = useState<Array<SnapType>>([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [userpair, setUserpair] = useState<string | null>(
+    localStorage.getItem("userpair") || ""
+  );
+  const [reversed, setReversed] = useState<string | null>(
+    localStorage.getItem("reversed") || ""
+  );
 
-type SnapType = {
-    creator: string,
-    displayDate: string,
-    image: string,
-    message: string,
-    timestamp: object,
-    userpair: string,
-    id: string
-}
-
-export const ChatProvider = ({children}: ChildrenType) => {
-    const [messages, setMessages] = useState<Array<SnapType>>([])
-    const [newMessage, setNewMessage] = useState("")
-    const [userpair, setUserpair] = useState<string | null>(localStorage.getItem("userpair") || "")
-    const [reversed, setReversed] = useState<string | null>(localStorage.getItem("reversed") || "")
-
-    const getMessages = useCallback(async (): Promise<SnapType[]> => {
-            try {
-                const messagedoc = collection(db, "messages")
-                const querymessages = query(messagedoc, where("userpair", "in", [`${userpair}`, `${reversed}`]), orderBy("timestamp"))
-                const snaps = await getDocs(querymessages)
-                let messagesarr: SnapType[] = []
-                let idarr: string[] = []
-                snaps.forEach((snap: any) => {
-                    messagesarr.push(snap.data())
-                    idarr.push(snap.id)
-                })
-                for (let i = 0; i < messagesarr.length; i++) {
-                    messagesarr[i].id = idarr[i]
-                }
-                return messagesarr
-            } catch (err) {
-                throw `${err} in the ChatContext in getMessages()`
-            }
-    }, [])
-
-    const handleSubmit = async (
-            e: React.FormEvent<HTMLFormElement>, 
-            creator: string, 
-            image: string | null | undefined, 
-            message: string, 
-            timestamp: FieldValue, 
-            userpair: string
-        ) => {
-        e.preventDefault()
-        try {
-            const docref = collection(db, "messages")
-            const displayDate: string = `${format(new Date(), 'MMMM dd, yyyy pp')}`
-            await addDoc(docref, {
-                creator,
-                image,
-                message,
-                timestamp,
-                userpair,
-                displayDate,
-                id: ""
-            })
-            setNewMessage("")
-            getMessages().then(setMessages)
-        } catch(err) {
-            console.log(`Error in ChatContext in handleSubmit: ${err}`)
-        }
+  const getMessages = useCallback(async (): Promise<SnapType[]> => {
+    try {
+      const messagedoc = collection(db, "messages");
+      const querymessages = query(
+        messagedoc,
+        where("userpair", "in", [`${userpair}`, `${reversed}`]),
+        orderBy("timestamp")
+      );
+      const snaps = await getDocs(querymessages);
+      let messagesarr: SnapType[] = [];
+      let idarr: string[] = [];
+      snaps.forEach((snap: any) => {
+        messagesarr.push(snap.data());
+        idarr.push(snap.id);
+      });
+      for (let i = 0; i < messagesarr.length; i++) {
+        messagesarr[i].id = idarr[i];
+      }
+      return messagesarr;
+    } catch (err) {
+      throw `${err} in the ChatContext in getMessages()`;
     }
+  }, []);
 
-    const handleDelete = useCallback(async (id: string) => {
-        try {
-            const docref = doc(db, "messages", id)
-            await deleteDoc(docref)
-        } catch (err) {
-            console.log(`Error in ChatContext in handleDelete(): ${err}`)
-        }
-    }, [])
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    creator: string,
+    image: string | null | undefined,
+    message: string,
+    timestamp: FieldValue,
+    userpair: string
+  ) => {
+    e.preventDefault();
+    try {
+      const docref = collection(db, "messages");
+      const displayDate: string = `${format(new Date(), "MMMM dd, yyyy pp")}`;
+      await addDoc(docref, {
+        creator,
+        image,
+        message,
+        timestamp,
+        userpair,
+        displayDate,
+        id: "",
+      });
+      setNewMessage("");
+      getMessages().then(setMessages);
+    } catch (err) {
+      console.log(`Error in ChatContext in handleSubmit: ${err}`);
+    }
+  };
 
-    useEffect(() => {
-        getMessages().then(setMessages)
-    }, [getMessages])
+  const handleDelete = useCallback(async (id: string) => {
+    try {
+      const docref = doc(db, "messages", id);
+      await deleteDoc(docref);
+    } catch (err) {
+      console.log(`Error in ChatContext in handleDelete(): ${err}`);
+    }
+  }, []);
 
-    return (
-        <ChatContext.Provider value={{messages, handleSubmit, newMessage, setNewMessage, setReversed, setUserpair, reversed, userpair, handleDelete}}>
-            {children}
-        </ChatContext.Provider>
-    )
-}
+  useEffect(() => {
+    getMessages().then(setMessages);
+  }, [getMessages]);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        messages,
+        handleSubmit,
+        newMessage,
+        setNewMessage,
+        setReversed,
+        setUserpair,
+        reversed,
+        userpair,
+        handleDelete,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
